fd013ff057b060a36083e581d6ee13cb67889005
Alexei <kr1loky@gmail.com>
2023-07-08 01:18:19 +0300

    Добавлено другое решение задачи 13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,12 +3,29 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-Console.WriteLine("Введите число");
+// Console.WriteLine("Введите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number >= 100)
+// {
+//     int thirdDigit = (number % 10);
+//     Console.WriteLine(thirdDigit);
+// }
+// else
+//     Console.WriteLine("Третьей цифры нет");
+
+Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
+if (number < 0) number *= -1;
 if (number >= 100)
 {
-    int thirdDigit = (number % 10);
-    Console.WriteLine(thirdDigit);
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine($"Третья цифра числа: {number % 10}");
 }
-else
-Console.WriteLine("Третьей цифры нет");
+else 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+    
