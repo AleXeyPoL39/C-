0f4a9d516378220f822abdcaf1988d27a2ffeee2
Alexei <kr1loky@gmail.com>
2023-07-18 20:49:17 +0300

    Добавление задачи 23

---
+++ Task23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите положительное целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N > 0)
+{
+   TableCube(N);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода");
+}
+
+static void TableCube(int Num)
+{
+    int count = 1;
+
+    while (count <= Num)
+    {
+        Console.WriteLine($"{count, 1} | {count * count * count}");
+        count++;
+    }
+}
